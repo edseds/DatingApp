38e7d55383051b2068ffb9674e08d089cb42a243
edseds <edgars.cirulis@gmail.com>
2020-03-16 22:39:43 +0200

    Login and Register frontend

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -59,11 +59,10 @@ namespace DatingApp.API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
     
             app.UseAuthentication();
             app.UseAuthorization();
-
-            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseEndpoints(endpoints =>
             {
